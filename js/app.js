135523f18a2f0d8b2080c3522e3a6d4858e66d28
Prateek <porwalp25@gmail.com>
2017-10-06 04:56:59 +0530

    added hamburger and list with markers

--- js/app.js
+++ js/app.js
@@ -1,33 +1,40 @@
 
 //array of places I would like to visit
 var places=[
-  {place:"Ozran,Goa",marker: markers[0]},
-  {place:"Arambol,Goa",marker: markers[1]},
-  {place:"Baga,Goa",marker: markers[2]},
-  {place:"Vagator,Goa",marker: markers[3]},
-  {place:"Anjuna,Goa",marker: markers[4]},
-  {place:"Chapora Fort,Goa",marker: markers[5]}
+  {place:"Ozran,Goa"},
+  {place:"Arambol,Goa"},
+  {place:"Baga,Goa"},
+  {place:"Vagator,Goa"},
+  {place:"Anjuna,Goa"},
+  {place:"Chapora Fort,Goa"}
 ];
 // View Model
 var PlaceViewModel = function() {
   self=this;
-  self.placeList=ko.observableArray(places);
-  self.filter = ko.observable("");
-  self.filteredData = ko.computed(function() {
+  self.placeList=ko.observableArray(places);    //observable array of places
+  self.filter = ko.observable("");                //empty obsservable
+  self.filteredData = ko.computed(function() {     //getting the list of filtered places
   var filter = self.filter().toLowerCase();
   if (!filter) {
     for(var i=0;i<markers.length;i++)
     {
-      markers[i].setMap(map);
+      markers[i].setMap(map);                //show all markers when there is no text in search box
     }
-    return places;
+    return places;                           //return all elements
   } else {
 
-    var filteredArray = ko.utils.arrayFilter(self.placeList(), function(item) {
+    var filteredArray = ko.utils.arrayFilter(self.placeList(), function(item) {   //filtered list when there is some text in search box
       return item.place.toLowerCase().indexOf(filter) > -1;
     });
+    if(filteredArray.length==0)                    //if nothing mathes hide all markers
+    {
+      for(var i=0;i<markers.length;i++)
+      {
+        markers[i].setMap(null);
+      }
+    }
 
-   for(var i=0;i<self.placeList().length;i++)
+   for(var i=0;i<self.placeList().length;i++)             //display markers for filtered places
     {
       for(var j=0;j<filteredArray.length;j++)
       {
@@ -38,27 +45,21 @@ var PlaceViewModel = function() {
         else
         {
             markers[i].setMap(map);
-            j=filteredArray.length;
+            j=filteredArray.length;                        //if place found then skip the remaining loop as it will set the map null again
         }
       }
     }
-  /*  for(var i=0;i<markers.length;i++)
-    {
-      markers[i].setMap(null);
-    }
-    for(var j=0;j<filteredArray.length;j++)
-    {
-      filteredArray[j].marker.setMap(map);
-    }*/
+
     return filteredArray;
 
 }
 });
 self.displayInfo = function(value)
 {
-  console.log(value.marker);
-   populateInfoWindow(value.marker,largeInfowindow);
+   console.log(value.place);
+   google.maps.event.trigger(value.marker, 'click');   //trigger click on marker if list element is clicked
    console.log("bvsjakjsj");
 };
+
 };
 ko.applyBindings(new PlaceViewModel());
